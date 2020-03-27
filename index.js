7583553cf871a422f9f051a9662052ca34d4b7fc
Victor <adevic4christ@gmail.com>
2020-03-27 19:15:51 +0100

    completed task

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+//Store your name in a variable
 const myName = "Adewumi Victor";
+//Store your courses in an array
 const myCourses = ["Node js", "php", "Javascript", "HTML", "CSS"];
+
+//Display name and courses
+console.log(myName);
+console.log(myCourses);
+
+//My courses' length
+const myCoursesLength = myCourses.length;
+
+//The logic: display odd numbers btw 1 and 200(inclusive) if courses' length is odd and otherwise if even
+for (let i = 0; i <= 200; i++) {
+  if (myCoursesLength % 2 === 0) {
+    i % 2 === 0 && console.log(i);
+  } else {
+    i % 2 !== 0 && console.log(i);
+  }
+}
